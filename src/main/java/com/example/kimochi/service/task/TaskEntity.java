2ecda2b32b1bd93898137fde31bfcfa1dd0d841a
omochi-star <omochim275@gmail.com>
2026-03-06 08:10:47 +0900

    フォームの入力値をDBに保存

--- src/main/java/com/example/kimochi/service/task/TaskEntity.java
+++ src/main/java/com/example/kimochi/service/task/TaskEntity.java
@@ -1,7 +1,7 @@
 package com.example.kimochi.service.task;
 
 public record TaskEntity(
-        long id,
+        Long id,
         String summary,
         String description,
         TaskStatus status
