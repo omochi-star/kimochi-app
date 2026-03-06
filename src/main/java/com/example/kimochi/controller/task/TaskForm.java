9a93965212424c7166f9e5f8640a1423cfeb04da
omochi-star <omochim275@gmail.com>
2026-03-06 21:03:36 +0900

    フォームのバリデーション

--- src/main/java/com/example/kimochi/controller/task/TaskForm.java
+++ src/main/java/com/example/kimochi/controller/task/TaskForm.java
@@ -2,14 +2,19 @@ package com.example.kimochi.controller.task;
 
 import com.example.kimochi.service.task.TaskEntity;
 import com.example.kimochi.service.task.TaskStatus;
+import jakarta.validation.constraints.NotBlank;
 
-public record TaskForm (
+public record TaskForm(
+        @NotBlank
         String summary,
+
         String description,
+
+        @NotBlank
         String status
-){
+) {
 
     public TaskEntity toEntity() {
-        return new TaskEntity(null,summary(),description(), TaskStatus.valueOf(status()));
+        return new TaskEntity(null, summary(), description(), TaskStatus.valueOf(status()));
     }
 }
