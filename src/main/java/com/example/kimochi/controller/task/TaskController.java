2d9c2923c6008eab704e6d3f98f284824e39e1bd
omochi-star <omochim275@gmail.com>
2026-03-08 14:58:36 +0900

    編集画面と作成画面でHTML生成を分けよう

--- src/main/java/com/example/kimochi/controller/task/TaskController.java
+++ src/main/java/com/example/kimochi/controller/task/TaskController.java
@@ -34,14 +34,15 @@ public class TaskController {
     }
 
     @GetMapping("/creationForm")
-    public String showCreationForm(@ModelAttribute TaskForm form) {
+    public String showCreationForm(@ModelAttribute TaskForm form,Model model) {
+        model.addAttribute("mode" ,"CREATE");
         return "tasks/form";
     }
 
     @PostMapping
-    public String create(@Validated TaskForm form, BindingResult bindingResult) {
+    public String create(@Validated TaskForm form, BindingResult bindingResult,Model model) {
         if (bindingResult.hasErrors()) {
-            return showCreationForm(form);
+            return showCreationForm(form,model);
         }
         taskService.create(form.toEntity());
         return "redirect:/tasks";
