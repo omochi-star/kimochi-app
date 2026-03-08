ee9ef5d557097399456d9db33b00c2e6e7706220
omochi-star <omochim275@gmail.com>
2026-03-08 15:55:39 +0900

    DB更新

--- src/main/java/com/example/kimochi/controller/task/TaskController.java
+++ src/main/java/com/example/kimochi/controller/task/TaskController.java
@@ -68,6 +68,8 @@ public class TaskController {
             model.addAttribute("mode","EDIT");
             return "tasks/form";
         }
+        var entity = form.toEntity(id);
+                taskService.update(entity);
         return "redirect:/tasks/{id}";
     }
 
--- src/main/java/com/example/kimochi/service/task/TaskService.java
+++ src/main/java/com/example/kimochi/service/task/TaskService.java
@@ -28,4 +28,8 @@ public class TaskService {
         taskRepository.insert(newEntity);
     }
 
+    @Transactional
+    public void update(TaskEntity entity) {
+        taskRepository.update(entity);
+    }
 }
